c689381c9693ce775121105a1d451804f5e26ba6
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-03-30 14:48:15 +1000

    correctly manipulate chronometer for start, pause, stop scenario

--- app/src/main/java/com/lachlanvass/sit305workouttimer/MainActivity.kt
+++ app/src/main/java/com/lachlanvass/sit305workouttimer/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.lachlanvass.sit305workouttimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
-import android.widget.EditText
-import android.widget.TextClock
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val start_button = findViewById<Button>(R.id.start_button)
+        val startButton = findViewById<Button>(R.id.start_button)
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        val stopButton = findViewById<Button>(R.id.stop_button)
 
+        var timeWhenStopped: Long = 0
         val timer = findViewById<Chronometer>(R.id.timer_display)
+        timer.format = "Time Running - %s"
         timer.base = SystemClock.elapsedRealtime()
 
-        start_button.setOnClickListener {
 
+        startButton.setOnClickListener {
+
+            timer.base = SystemClock.elapsedRealtime() + timeWhenStopped;
             timer.start()
         }
+
+        pauseButton.setOnClickListener {
+
+            timeWhenStopped = timer.base - SystemClock.elapsedRealtime();
+            timer.stop()
+            println("Pause button")
+        }
+
+        stopButton.setOnClickListener {
+
+            timer.stop()
+            timer.base = SystemClock.elapsedRealtime();
+            timeWhenStopped = 0;
+        }
+
+
     }
 }
