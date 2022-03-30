da0a1823dd710812a9365f3be7ed7d72a6f5294e
lachlan-vass <lachlan.vass@superretailgroup.com>
2022-03-30 15:56:10 +1000

    format output time

--- app/src/main/java/com/lachlanvass/sit305workouttimer/MainActivity.kt
+++ app/src/main/java/com/lachlanvass/sit305workouttimer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         timer.format = "Time Running - %s"
         timer.base = SystemClock.elapsedRealtime()
 
+        val workoutSummary = findViewById<TextView>(R.id.workout_summary)
 
         startButton.setOnClickListener {
 
@@ -37,6 +39,13 @@ class MainActivity : AppCompatActivity() {
         stopButton.setOnClickListener {
 
             timer.stop()
+
+            timeWhenStopped = SystemClock.elapsedRealtime() - timer.base
+
+            val minutes = timeWhenStopped / 1000 / 60
+            val seconds = timeWhenStopped / 1000 % 60
+
+            workoutSummary.text = "${workoutSummary.text} TIME: $minutes:$seconds "
             timer.base = SystemClock.elapsedRealtime();
             timeWhenStopped = 0;
         }
